2c3433d1b9c1327a29c50b89f15b9db2bf4b348f
Kikolock <icaro12@hotmail.com>
2022-09-18 12:10:53 -0400

    Password criteria + Random formula added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,57 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+var passwordCriteria = {
+  size: 0,
+  lowercase: 0,
+  uppercase: 0,
+  numeric: 0,
+  special: 0
+}
+
 function writePassword() {
+  criteriaPrompts ();
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
-// Add event listener to generate button
+
+var generatePassword = function() {
+  var newPassword = "";
+  for (i = 0; i < passwordCriteria.size; i++){
+    var charType = randomNumber(1, 4);
+
+    
+    if (charType === 1 && passwordCriteria.lowercase === 1) {
+      newPassword += genLowercase();
+      console.log(newPassword);
+    }
+    else if (charType === 2 && passwordCriteria.uppercase === 1) {
+      newPassword += genUppercase();
+      console.log(newPassword);
+    }
+    else if (charType === 3 && passwordCriteria.numeric === 1) {
+      newPassword += genNumeric();
+      console.log(newPassword);
+    }
+    else if (charType === 4 && passwordCriteria.special === 1) {
+      newPassword += genSpecial();
+      console.log(newPassword);
+    }
+    
+    else i--;
+    
+  }
+  return newPassword;
+}
+
+var randomNumber = function(min, max) {
+  var value = Math.floor(Math.random() * (max - min + 1) + min);
+
+  return value;
+}
+
+
 generateBtn.addEventListener("click", writePassword);
